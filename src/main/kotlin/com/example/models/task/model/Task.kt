7cb3028f41e815696dba067e9ec274f31f1fed34
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-08-25 19:18:59 +0300

    Added new property id and add an object used for its serialization.

--- src/main/kotlin/com/example/models/task/model/Task.kt
+++ src/main/kotlin/com/example/models/task/model/Task.kt
@@ -1,10 +1,36 @@
 package com.example.models.task.model
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.util.*
 
 enum class Priority {
     low, medium, high, vital
 }
 
+// Object used to serialize UUID.
+object UUIDSerializer : KSerializer<UUID> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: UUID) {
+        encoder.encodeString(value.toString())
+    }
+
+    override fun deserialize(decoder: Decoder): UUID {
+        return UUID.fromString(decoder.decodeString())
+    }
+}
+
 @Serializable
-data class Task(val name: String, val description: String, val priority: Priority)
+data class Task(
+    @Serializable(with = UUIDSerializer::class)
+    val id: UUID,
+    val name: String,
+    val description: String,
+    val priority: Priority
+)
