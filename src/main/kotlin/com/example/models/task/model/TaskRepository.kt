1768c2e81a11e54353083dd07db0e43607a16fb1
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-08-25 18:42:29 +0300

    Added the TaskRepository.getTasks.

--- src/main/kotlin/com/example/models/task/model/TaskRepository.kt
+++ src/main/kotlin/com/example/models/task/model/TaskRepository.kt
@@ -1,5 +1,24 @@
 package com.example.models.task.model
 
+import com.example.models.user.model.User
+import com.example.models.user.model.UserRepository
+import com.example.tables.TaskTable
+import org.jetbrains.exposed.sql.transactions.transaction
+
 // Repository used to get task data from the table.
 object TaskRepository {
+    // Method that will return all task of a user if the credentials are correct.
+    fun getTasks(user: User): List<Task>? {
+        return transaction {
+            val id = UserRepository.getUserId(user)
+            id?.let {
+                TaskTable
+                    .select(TaskTable.name, TaskTable.description, TaskTable.priority)
+                    .where { TaskTable.userId eq id }
+                    .map {
+                        Task(it[TaskTable.name], it[TaskTable.description], Priority.valueOf(it[TaskTable.priority]))
+                    }
+            }
+        }
+    }
 }
