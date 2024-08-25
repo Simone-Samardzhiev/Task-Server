91e0596cfdef0f76db3bb2916b187d3a3ebe079d
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-08-25 20:45:26 +0300

    Added the route tasks and the get method for getting tasks.

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,6 +1,7 @@
 package com.example.plugins
 
 import com.example.models.errors.model.ErrorResponse
+import com.example.models.task.model.TaskRepository
 import com.example.models.user.model.User
 import com.example.models.user.model.UserRepository
 import io.ktor.http.*
@@ -35,6 +36,34 @@ fun Application.configureRouting() {
                    )
                }
            }
+        }
+        route("/tasks") {
+            get {
+                try {
+                    val user = call.receive<User>()
+
+                    val tasks = TaskRepository.getTasks(user)
+
+                    tasks?.let {
+                        call.respond(HttpStatusCode.OK, it)
+                    } ?: call.respond(
+                        HttpStatusCode.Unauthorized,
+                        ErrorResponse(
+                            HttpStatusCode.Unauthorized.value,
+                            "You email or password is wrong."
+                        )
+                    )
+
+                } catch (e: IllegalArgumentException) {
+                    call.respond(
+                        HttpStatusCode.BadRequest,
+                        ErrorResponse(
+                            HttpStatusCode.BadRequest.value,
+                            "The json file could not be read."
+                        )
+                    )
+                }
+            }
         }
     }
 }
