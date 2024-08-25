abdbf5ee5ffa481907af8da69bf84e261a30a8ff
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-08-25 16:29:42 +0300

    Add UserRepository.checkIfEmailInUse method.

--- src/main/kotlin/com/example/models/user/model/UserRepository.kt
+++ src/main/kotlin/com/example/models/user/model/UserRepository.kt
@@ -1,5 +1,17 @@
 package com.example.models.user.model
 
-object UserRepository {
+import com.example.tables.UserTable
+import org.jetbrains.exposed.sql.transactions.transaction
 
+// Repository used to get user data from the table.
+object UserRepository {
+    // Method that will check if a user with this email exist.
+    suspend fun checkIfEmailInUse(email:String): Boolean {
+        return transaction {
+            UserTable
+                .select(UserTable.email)
+                .where { UserTable.email eq email }
+                .count() > 0
+        }
+    }
 }
