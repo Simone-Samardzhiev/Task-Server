dac0f39a80a7fcb25ff7861b95658e92b15ab5f2
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-08-25 18:42:09 +0300

    Added the priority property and made the size of all varchar to 50 in TaskTable.

--- src/main/kotlin/com/example/tables/TaskTable.kt
+++ src/main/kotlin/com/example/tables/TaskTable.kt
@@ -10,9 +10,11 @@ object TaskTable : Table("tasks") {
     // The id of the task.
     val id: Column<UUID> = uuid("id")
     // The name of the task.
-    val name: Column<String> = varchar("name", 255)
+    val name: Column<String> = varchar("name", 50)
     // The description of the task.
-    val description: Column<String> = varchar("description", 255)
+    val description: Column<String> = varchar("description", 50)
+    // The priority of the task.
+    val priority: Column<String> = varchar("priority", 50)
     // Foreign key that reference a user id.
     val userId :Column<UUID> = reference("user_id", UserTable.id)
     // Setting the primary key of the task.
