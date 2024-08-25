cf49ca21f38f7afca1ce21ef4d26ad5517ff28fc
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-08-25 22:44:28 +0300

    Refactored the code to use query parameter.

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,7 +1,9 @@
 package com.example.plugins
 
-import com.example.models.response.models.ErrorResponse
+
+import com.example.models.ErrorResponse
 import com.example.models.task.models.TaskRepository
+import com.example.models.task.models.TaskWithoutId
 import com.example.models.user.models.User
 import com.example.models.user.models.UserRepository
 import io.ktor.http.*
@@ -14,35 +16,47 @@ fun Application.configureRouting() {
     routing {
         route("/users") {
             post("/register") {
-                try {
-                    val user = call.receive<User>()
-                    if (UserRepository.registerUser(user)) {
-                        call.respond(HttpStatusCode.OK)
-                    } else {
-                        call.respond(
-                            HttpStatusCode.Conflict,
-                            ErrorResponse(
-                                HttpStatusCode.Conflict.value,
-                                "The email is already in use."
-                            )
-                        )
-                    }
-                } catch (e: IllegalArgumentException) {
+                val email = call.request.queryParameters["email"]
+                val password = call.request.queryParameters["password"]
+
+                if (email == null || password == null) {
                     call.respond(
                         HttpStatusCode.BadRequest,
                         ErrorResponse(
                             HttpStatusCode.BadRequest.value,
-                            "The json file could not be read."
+                            "The query parameters email and password must be specified."
                         )
                     )
+                } else {
+                    if (UserRepository.registerUser(User(email, password))) {
+                        call.respond(HttpStatusCode.Created)
+                    } else {
+                        call.respond(
+                            HttpStatusCode.Conflict,
+                            ErrorResponse(
+                                HttpStatusCode.BadRequest.value,
+                                "The email is already in use."
+                            )
+                        )
+                    }
                 }
             }
         }
         route("/tasks") {
             get {
-                try {
-                    val user = call.receive<User>()
-                    val tasks = TaskRepository.getTasks(user)
+                val email = call.request.queryParameters["email"]
+                val password = call.request.queryParameters["password"]
+
+                if (email == null || password == null) {
+                    call.respond(
+                        HttpStatusCode.BadRequest,
+                        ErrorResponse(
+                            HttpStatusCode.BadRequest.value,
+                            "The query parameters email and password must be specified."
+                        )
+                    )
+                } else {
+                    val tasks = TaskRepository.getTasks(User(email, password))
 
                     tasks?.let {
                         call.respond(HttpStatusCode.OK, it)
@@ -50,18 +64,50 @@ fun Application.configureRouting() {
                         HttpStatusCode.Unauthorized,
                         ErrorResponse(
                             HttpStatusCode.Unauthorized.value,
-                            "You email or password is wrong."
+                            "The email or the password is invalid."
                         )
                     )
+                }
+            }
 
-                } catch (e: IllegalArgumentException) {
+            post("/addTask") {
+                val email = call.request.queryParameters["email"]
+                val password = call.request.queryParameters["password"]
+
+                if (email == null || password == null) {
                     call.respond(
                         HttpStatusCode.BadRequest,
                         ErrorResponse(
                             HttpStatusCode.BadRequest.value,
-                            "The json file could not be read."
+                            "The query parameters email and password must be specified."
                         )
                     )
+
+                } else {
+                    try {
+                        val task = call.receive<TaskWithoutId>()
+
+                        if (TaskRepository.addTask(User(email, password), task)) {
+                            call.respond(HttpStatusCode.NoContent)
+                        } else {
+                            call.respond(
+                                HttpStatusCode.Unauthorized,
+                                ErrorResponse(
+                                    HttpStatusCode.Unauthorized.value,
+                                    "The email or the password is invalid."
+                                )
+                            )
+                        }
+
+                    } catch (e: IllegalArgumentException) {
+                        call.respond(
+                            HttpStatusCode.BadRequest,
+                            ErrorResponse(
+                                HttpStatusCode.BadRequest.value,
+                                "The json file could not be read."
+                            )
+                        )
+                    }
                 }
             }
         }
