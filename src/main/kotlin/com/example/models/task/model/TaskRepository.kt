413e196e49d7eddc14256e7e73c9cceb92faf629
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-08-25 19:04:17 +0300

    Added TaskRepository.addTask.

--- src/main/kotlin/com/example/models/task/model/TaskRepository.kt
+++ src/main/kotlin/com/example/models/task/model/TaskRepository.kt
@@ -3,7 +3,9 @@ package com.example.models.task.model
 import com.example.models.user.model.User
 import com.example.models.user.model.UserRepository
 import com.example.tables.TaskTable
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.UUID
 
 // Repository used to get task data from the table.
 object TaskRepository {
@@ -19,6 +21,24 @@ object TaskRepository {
                         Task(it[TaskTable.name], it[TaskTable.description], Priority.valueOf(it[TaskTable.priority]))
                     }
             }
+        }
+    }
+
+    // Method that will add a task if the user credentials are correct.
+    fun addTask(user: User, task: Task): Boolean {
+        return transaction {
+            val userId = UserRepository.getUserId(user)
+            userId?.let {
+                TaskTable
+                    .insert {
+                        it[id] = UUID.randomUUID()
+                        it[name] = task.name
+                        it[description] = task.description
+                        it[priority] = task.priority.name
+                        it[user_id] = userId
+                    }
+                true
+            } ?: false
         }
     }
 }
