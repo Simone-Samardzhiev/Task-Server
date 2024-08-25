cb4c0903c2d095319f0faf88b9f8a8bd9de87865
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-08-25 16:05:10 +0300

    Made both Task and User be serializable.

--- src/main/kotlin/com/example/models/Task.kt
+++ src/main/kotlin/com/example/models/Task.kt
@@ -1,7 +1,10 @@
 package com.example.models
 
+import kotlinx.serialization.Serializable
+
 enum class Priority {
     low, medium, high, vital
 }
 
+@Serializable
 data class Task(val name: String, val description: String, val priority: Priority)
--- src/main/kotlin/com/example/models/User.kt
+++ src/main/kotlin/com/example/models/User.kt
@@ -1,3 +1,6 @@
 package com.example.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class User(val name: String, val password: String)
