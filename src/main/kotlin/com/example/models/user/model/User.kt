904fce888195f300f12de33bd126c4d23ece27cf
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-08-25 16:39:41 +0300

    Renamed name to email in data class User.

--- src/main/kotlin/com/example/models/user/model/User.kt
+++ src/main/kotlin/com/example/models/user/model/User.kt
@@ -3,4 +3,4 @@ package com.example.models.user.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class User(val name: String, val password: String)
+data class User(val email: String, val password: String)
