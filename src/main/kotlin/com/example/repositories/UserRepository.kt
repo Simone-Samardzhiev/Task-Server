f81052cc78fe900d4a9c86188a95dac5236507ca
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-09-18 16:18:25 +0300

    Add UserRepository.kt

---
+++ src/main/kotlin/com/example/repositories/UserRepository.kt
@@ -0,0 +1,12 @@
+package com.example.repositories
+
+import com.example.models.User
+import com.example.tables.UserTable
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.mindrot.jbcrypt.BCrypt
+
+// Object that will manage the data in of the users
+object UserRepository {
+
+}
