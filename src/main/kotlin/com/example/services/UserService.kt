01d41851a61ce71c0af2a522933c40a0a05b1c63
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-09-18 17:58:11 +0300

    Refactor UserService.isValidEmail

--- src/main/kotlin/com/example/services/UserService.kt
+++ src/main/kotlin/com/example/services/UserService.kt
@@ -1,11 +1,13 @@
 package com.example.services
+import com.example.models.User
+import com.example.repositories.UserRepository
 
 // Service that will manage the users
 object UserService {
     // Method that will check if the email is valid.
     private fun isValidEmail(email: String): Boolean {
         val regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$".toRegex()
-        return email.isBlank() && email.matches(regex)
+        return email.isBlank() && email.matches(regex) && !UserRepository.checkIfEmailExists(email)
     }
 
     // Method that will check if the password if valid.
@@ -13,4 +15,5 @@ object UserService {
         val regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)(?=.*[@#$%^&+=])(?=\\\\S+\\$).{8,}$".toRegex()
         return password.matches(regex)
     }
+
 }
