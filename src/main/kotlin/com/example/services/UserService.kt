221d5a49965cf2f34dff7ffe5957b540f783f0dc
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-09-18 17:54:43 +0300

    Add UserService.isValidEmail

--- src/main/kotlin/com/example/services/UserService.kt
+++ src/main/kotlin/com/example/services/UserService.kt
@@ -2,5 +2,10 @@ package com.example.services
 
 // Service that will manage the users
 object UserService {
+    // Method that will check if the email is valid.
+    private fun isValidEmail(email: String): Boolean {
+        val regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$".toRegex()
+        return email.isBlank() && email.matches(regex)
+    }
 
 }
