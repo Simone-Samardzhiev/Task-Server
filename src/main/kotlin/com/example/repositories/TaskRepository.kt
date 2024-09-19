c5c28ade93d480b4a4c433ca5a9b159d797799cc
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-09-19 11:17:52 +0300

    Add TaskRepository.addTask

--- src/main/kotlin/com/example/repositories/TaskRepository.kt
+++ src/main/kotlin/com/example/repositories/TaskRepository.kt
@@ -2,19 +2,21 @@ package com.example.repositories
 
 import com.example.models.Priority
 import com.example.models.Task
+import com.example.models.TaskWithoutId
 import com.example.tables.TaskTable
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.UUID
 
 // Object that will manage the data of tasks
 object TaskRepository {
     // Method that will return the tasks that belong to a user
-    fun getTasks(id: UUID): List<Task> {
+    fun getTasks(userId: UUID): List<Task> {
         val result = mutableListOf<Task>()
         transaction {
             TaskTable
                 .select(TaskTable.id, TaskTable.name, TaskTable.description, TaskTable.priority)
-                .where { TaskTable.user_id eq id }
+                .where { TaskTable.user_id eq userId }
                 .map {
                     result.add(
                         Task(
@@ -27,5 +29,18 @@ object TaskRepository {
                 }
         }
         return result
+    }
+
+    // Method that will add a new task
+    fun addTask(task: TaskWithoutId, userId: UUID) {
+        transaction {
+            TaskTable
+                .insert {
+                    it[id] = UUID.randomUUID()
+                    it[name] = task.name
+                    it[description] = task.description
+                    it[priority] = task.priority.name
+                }
+        }
     }
 }
