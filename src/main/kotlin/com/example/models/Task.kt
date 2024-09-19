452f195cad64770329f60620a25f3c4ac8ef78e7
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-09-19 14:36:26 +0300

    Add UUIDSerializer

--- src/main/kotlin/com/example/models/Task.kt
+++ src/main/kotlin/com/example/models/Task.kt
@@ -1,6 +1,12 @@
 package com.example.models
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import java.util.UUID
 
 // Enum class used to create task priority
@@ -8,9 +14,23 @@ enum class Priority {
     Low, Medium, High, Vital
 }
 
+// Custom serializer for UUID
+object UUIDSerializer : KSerializer<UUID> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: UUID) {
+        encoder.encodeString(value.toString())
+    }
+
+    override fun deserialize(decoder: Decoder): UUID {
+        return UUID.fromString(decoder.decodeString())
+    }
+}
+
 // Class used to create tasks
 @Serializable
 data class Task(
+    @Serializable(with = UUIDSerializer::class)
     val id: UUID,
     val name: String,
     val description: String,
