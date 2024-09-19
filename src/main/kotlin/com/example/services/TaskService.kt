5518d2142055775ee721ce45c4add994682bb7aa
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-09-19 12:02:27 +0300

    Add TaskService.getTasks

--- src/main/kotlin/com/example/services/TaskService.kt
+++ src/main/kotlin/com/example/services/TaskService.kt
@@ -6,5 +6,8 @@ import java.util.UUID
 
 // Service that will manage the tasks
 object TaskService {
-
+    // Method that will get the user tasks from the repository
+    fun getTasks(userId: UUID) : List<Task> {
+        return TaskRepository.getTasks(userId)
+    }
 }
