c425702aec5fadd6b76d24a1bf76a08f8325c918
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-09-19 12:03:26 +0300

    Add TaskService.addTask

--- src/main/kotlin/com/example/services/TaskService.kt
+++ src/main/kotlin/com/example/services/TaskService.kt
@@ -1,6 +1,7 @@
 package com.example.services
 
 import com.example.models.Task
+import com.example.models.TaskWithoutId
 import com.example.repositories.TaskRepository
 import java.util.UUID
 
@@ -9,5 +10,10 @@ object TaskService {
     // Method that will get the user tasks from the repository
     fun getTasks(userId: UUID) : List<Task> {
         return TaskRepository.getTasks(userId)
+    }
+
+    // Method that will add a task to the repository
+    fun addTask(task: TaskWithoutId, userId: UUID) {
+        TaskRepository.addTask(task, userId)
     }
 }
