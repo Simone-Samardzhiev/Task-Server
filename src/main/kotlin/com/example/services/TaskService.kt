c8cddd3aa3815a718bbd252a5977b42c0aa9986c
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-09-19 11:39:02 +0300

    Add TaskService.kt

---
+++ src/main/kotlin/com/example/services/TaskService.kt
@@ -0,0 +1,10 @@
+package com.example.services
+
+import com.example.models.Task
+import com.example.repositories.TaskRepository
+import java.util.UUID
+
+// Service that will manage the tasks
+object TaskService {
+
+}
