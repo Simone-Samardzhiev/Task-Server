b70088d8516e068af90ae4e3ce5d608adbeeb408
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-09-30 17:45:49 +0300

    Add UserTable.kt

---
+++ src/main/kotlin/com/example/user/UserTable.kt
@@ -0,0 +1,13 @@
+package com.example.user
+
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
+import java.util.UUID
+
+// Table used to get data about users
+object UserTable: Table("users") {
+    val id: Column<UUID> = uuid("id")
+    val email: Column<String> = varchar("email", 255).uniqueIndex()
+    val password: Column<String> = varchar("password", 255)
+    override val primaryKey = PrimaryKey(id)
+}
