deed7b9f6df91a548e90ac9ec00f79d6185e2c54
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-09-30 23:42:22 +0300

    Add validateEmail method to UserService

--- src/main/kotlin/com/example/user/UserService.kt
+++ src/main/kotlin/com/example/user/UserService.kt
@@ -1,4 +1,10 @@
 package com.example.user
 
-object UserService {
+// Service used to manage the data of the users
+object UserService: UserServiceInterface {
+    // Method that will check for valid email syntax
+    override suspend fun validateEmail(email: String): Boolean {
+        val regex = "^[\\w._%+0-]+@[\\w._-]+\\.[a-zA-Z]{2,}$".toRegex()
+        return email.matches(regex)
+    }
 }
--- src/main/kotlin/com/example/user/UserServiceInterface.kt
+++ src/main/kotlin/com/example/user/UserServiceInterface.kt
@@ -1,4 +1,7 @@
 package com.example.user
 
+// Interface used to create user service
 interface UserServiceInterface {
+    // Method that will check for valid email syntax
+    suspend fun validateEmail(email: String): Boolean
 }
