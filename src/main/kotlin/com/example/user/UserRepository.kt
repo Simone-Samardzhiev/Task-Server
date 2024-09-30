4741da004a0d2c30f5291a5aa047147b619a49e4
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-09-30 17:57:39 +0300

    Add checkEmail method to UserRepository

--- src/main/kotlin/com/example/user/UserRepository.kt
+++ src/main/kotlin/com/example/user/UserRepository.kt
@@ -1,4 +1,17 @@
 package com.example.user
 
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+
+// User repository that will manage the data of the users
 object UserRepository: UserRepositoryInterface {
+    // Method used to check if an email exist
+    override suspend fun checkEmail(email: String): Boolean {
+        return transaction {
+            UserTable
+                .select(UserTable.email)
+                .where { UserTable.email eq email }
+                .count() > 0
+        }
+    }
 }
--- src/main/kotlin/com/example/user/UserRepositoryInterface.kt
+++ src/main/kotlin/com/example/user/UserRepositoryInterface.kt
@@ -1,5 +1,7 @@
 package com.example.user
 
+// Interface used to create user repository
 interface UserRepositoryInterface {
-
+    // Method used to check if an email exist
+    suspend fun checkEmail(email: String): Boolean
 }
