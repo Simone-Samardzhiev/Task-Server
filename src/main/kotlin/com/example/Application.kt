dfd8b8a5e0176e41db1569d2cee55ec5fb607a1a
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-09-30 16:41:00 +0300

    Remove configureDatabase method for Application

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,6 +1,6 @@
 package com.example
 
-import com.example.plugins.configureDatabase
+
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -11,5 +11,5 @@ fun main() {
 }
 
 fun Application.module() {
-    configureDatabase()
+
 }
