756813359f12699062f3c83b94ca738c51eed7a3
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-07 16:12:16 +0300

    Add comments and documentation to Security.kt

--- src/main/kotlin/com/example/plugins/Security.kt
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -7,19 +7,22 @@ import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 
+/**
+ * Extension function used to configure the jwt authentication if the server.
+ */
 fun Application.configureSecurity(jwtUserService: JWTUserServiceInterface) {
     install(Authentication) {
         jwt {
-            realm = jwtUserService.realm
+            realm = jwtUserService.realm // Setting the realm
             verifier(
                 JWT
-                    .require(Algorithm.HMAC256(jwtUserService.secret))
-                    .withAudience(jwtUserService.audience)
-                    .withIssuer(jwtUserService.issuer)
-                    .build()
+                    .require(Algorithm.HMAC256(jwtUserService.secret)) // Setting the hash algorithm
+                    .withAudience(jwtUserService.audience) // Setting the audience
+                    .withIssuer(jwtUserService.issuer) // Setting the issuer
+                    .build() // Building
             )
             validate { credentials ->
-                jwtUserService.validateCredentials(credentials)
+                jwtUserService.validateCredentials(credentials) // Validation the credentials
             }
         }
     }
