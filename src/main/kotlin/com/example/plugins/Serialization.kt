9e25ed754ac29e186fe54b372a8935c272c81917
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-07 16:20:08 +0300

    Add comments and documentation to Serialization.kt

--- src/main/kotlin/com/example/plugins/Serialization.kt
+++ src/main/kotlin/com/example/plugins/Serialization.kt
@@ -5,6 +5,9 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 
+/**
+ * Extension function that will set the content negotiation.
+ */
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json()
