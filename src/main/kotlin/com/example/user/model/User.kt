023d886829f6df1d60022dbd71b52cb004e7cae3
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-07 16:48:24 +0300

    Add comments and documentation to User.kt

--- src/main/kotlin/com/example/user/model/User.kt
+++ src/main/kotlin/com/example/user/model/User.kt
@@ -2,5 +2,10 @@ package com.example.user.model
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Class used to create user.
+ * @param email The email of the user.
+ * @param password The password of the user.
+ */
 @Serializable
 data class User(val email: String, val password: String)
