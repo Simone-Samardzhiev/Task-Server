20705e1124461253e91518a60e3525d552be361a
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-08 08:32:06 +0300

    Add getTasks method and fixed the bug where the TaskServiceInterface will use UserRepository instead of TaskRepository

--- src/main/kotlin/com/example/task/service/TaskService.kt
+++ src/main/kotlin/com/example/task/service/TaskService.kt
@@ -1,7 +1,11 @@
 package com.example.task.service
 
-import com.example.user.repository.UserRepositoryInterface
+import com.example.task.model.Task
+import com.example.task.repository.TaskRepositoryInterface
+import java.util.UUID
 
 
-class TaskService(override val userRepository: UserRepositoryInterface): TaskServiceInterface {
+class TaskService(override val taskRepository: TaskRepositoryInterface) : TaskServiceInterface {
+    override suspend fun getTasks(userId: UUID): List<Task> = taskRepository.getTasksByUserId(userId)
+
 }
--- src/main/kotlin/com/example/task/service/TaskServiceInterface.kt
+++ src/main/kotlin/com/example/task/service/TaskServiceInterface.kt
@@ -1,11 +1,21 @@
 package com.example.task.service
 
+import com.example.task.model.Task
+import com.example.task.repository.TaskRepositoryInterface
 import com.example.user.repository.UserRepositoryInterface
+import java.util.UUID
 
 /**
  * Interface used to create task service.
- * @property userRepository The task repository.
+ * @property taskRepository The task repository.
  */
 interface TaskServiceInterface {
-    val userRepository: UserRepositoryInterface
+    val taskRepository: TaskRepositoryInterface
+
+    /**
+     * Method that will get all tasks that belongs to a user.
+     * @param userId The user id.
+     * @return List of the task that where retrieved.
+     */
+    suspend fun getTasks(userId: UUID): List<Task>
 }
