0adcb495ff177a5f787bd546b9faaa439d44b243
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-08 08:37:50 +0300

    Add addTask method in TaskService

--- src/main/kotlin/com/example/task/service/TaskService.kt
+++ src/main/kotlin/com/example/task/service/TaskService.kt
@@ -1,11 +1,24 @@
 package com.example.task.service
 
 import com.example.task.model.Task
+import com.example.task.model.TaskWithoutId
 import com.example.task.repository.TaskRepositoryInterface
 import java.util.UUID
 
 
 class TaskService(override val taskRepository: TaskRepositoryInterface) : TaskServiceInterface {
     override suspend fun getTasks(userId: UUID): List<Task> = taskRepository.getTasksByUserId(userId)
+    override suspend fun addTask(task: TaskWithoutId, userId: UUID) {
+        val task = Task(
+            id = UUID.randomUUID(),
+            name = task.name,
+            description = task.description,
+            priority = task.priority,
+            dueDate = task.dueDate,
+            dateDeleted = task.dateDeleted,
+            dateCompleted = task.dateCompleted
+        )
 
+        taskRepository.addTask(task, userId)
+    }
 }
--- src/main/kotlin/com/example/task/service/TaskServiceInterface.kt
+++ src/main/kotlin/com/example/task/service/TaskServiceInterface.kt
@@ -1,6 +1,7 @@
 package com.example.task.service
 
 import com.example.task.model.Task
+import com.example.task.model.TaskWithoutId
 import com.example.task.repository.TaskRepositoryInterface
 import com.example.user.repository.UserRepositoryInterface
 import java.util.UUID
@@ -18,4 +19,10 @@ interface TaskServiceInterface {
      * @return List of the task that where retrieved.
      */
     suspend fun getTasks(userId: UUID): List<Task>
+
+    /**
+     * Method that will add a new task.
+     * @param task The task that will be added.
+     */
+    suspend fun addTask(task: TaskWithoutId, userId: UUID)
 }
