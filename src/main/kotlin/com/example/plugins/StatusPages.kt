b1c315a5586240b9d360a73b75af14899150e163
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-17 14:30:18 +0300

    Add StatusPages.kt

---
+++ src/main/kotlin/com/example/plugins/StatusPages.kt
@@ -0,0 +1,15 @@
+package com.example.plugins
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.respond
+import kotlinx.serialization.SerializationException
+
+fun Application.configureStatusPages() {
+    install(StatusPages) {
+        exception<SerializationException> { call, cause ->
+            call.respond(HttpStatusCode.BadRequest, "The json data in not valid!")
+        }
+    }
+}
